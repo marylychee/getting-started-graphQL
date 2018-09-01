c26f1a0688ca49f641dedb3e30dd15cfb59edf11
Mary Lai <marylychee@gmail.com>
2018-09-01 10:57:49 +1000

    graphql schema in js

--- index.js
+++ index.js
@@ -2,25 +2,65 @@
 
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const { graphql, buildSchema } = require('graphql');
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLBoolean,
+} = require('graphql');
 
 const PORT = process.env.PORT || 3000;
 const server = express();
 
-// query feild type of foo which is a type of string
-const schema = buildSchema(`
-type Video {
-  id: ID,
-  title: String,
-  duration: Int,
-  watched: Boolean
-}
+const videoType = new GraphQLObjectType({
+  name: 'Video',
+  description: 'A Video on a Egghead.io',
+  fields: {
+    id: {
+      type: GraphQLID,
+      description: 'The id of the video'
+    },
+    title: {
+      type: GraphQLString,
+      description: 'The title of the video'
+    },
+    duration: {
+      type: GraphQLInt,
+      description: 'The duration of the video in seconds'
+    },
+    watched: {
+      type: GraphQLBoolean,
+      description: 'Whether or not the viewer has watched the video'
+    }
+  },
+});
 
-type Query {
-  video: Video,
-  videos: [Video]
-}
-`);
+const queryType = new GraphQLObjectType({
+  name: 'QueryType',
+  description: 'The root query type',
+  fields: {
+    video: {
+      type: videoType,
+      resolve: () => new Promise((resolve) => {
+        // represent a video
+        resolve({
+          id: 'a',
+          title: 'GraphQL',
+          duration: 180,
+          watched: false,
+        })
+      })
+    }
+  }
+})
+
+const schema = new GraphQLSchema({
+  query: queryType,
+  // mutation,
+  // subscription
+});
 
 const videoA = {
   id: 'a',
@@ -38,21 +78,11 @@ const videoB = {
 
 const videos = [videoA, videoB];
 
-const resolvers = {
-  video: () => ({
-    id: () => 1,
-    title: () => 'bar',
-    duration: () => 180,
-    watched: () => true,
-  }),
-  videos: () => videos,
-};
-
+// graphql http middleware function
 server.use('/graphql', graphqlHTTP({
   // config object
-  schema,
-  graphiql: true,
-  rootValue: resolvers
+  schema, // schema above
+  graphiql: true, // visual IDE
 }));
 
 server.listen(PORT, () => {
